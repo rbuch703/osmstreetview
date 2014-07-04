d0f79de5a5f7c907e62c25d8416653ee9bdde10f
Robert Buchholz <rbuch703@gmail.com>
2014-07-04 12:07:37 +0200

    - refactoring: extracting controller code into a separate class

---
+++ controller.js
@@ -0,0 +1,224 @@
+"use strict"    
+
+
+var eye,position;
+var localPosition = { x:0, y:0 }; //camera position in the local coordinate system
+var viewAngleYaw = 0;
+var viewAnglePitch = 0;
+
+
+var Controller = {
+    buildQueryString: function()
+    {
+        return "?lat="+position.lat+"&lng="+position.lng+"&yaw="+viewAngleYaw.toFixed(1)+"&pitch="+viewAnglePitch.toFixed(1);
+    },
+
+    initFromQueryString: function(queryString)
+    {
+        var query = this.toDictionary(queryString);
+        if (query.lat && query.lng)
+        {
+            position = {lat:query.lat, lng:query.lng};
+            
+            if ( ("yaw" in  query) && ("pitch" in query) ) //if look direction is also given
+            {
+                viewAngleYaw = query.yaw;
+                viewAnglePitch=query.pitch;
+            }
+        }
+    },
+
+    toDictionary: function(queryString)
+    {
+        var parts = queryString.split("&");
+        var res = {};
+        for (var i in parts)
+        {
+            var part = parts[i];
+            var kv = part.split("=");
+            if (kv.length == 2)
+            {
+                res[kv[0]] = parseFloat(kv[1]);
+            }
+        }
+        return res;
+    },
+    
+    
+
+	onMouseDown: function(e)
+	{
+	    if (e.button != 0) 
+	        return;
+
+	    this.x = e.clientX;
+	    this.y = e.clientY;
+	    this.down = "mouse";
+	},
+	
+	onMouseUp: function(e)
+	{
+	    if (e.button != 0) 
+	        return;
+	    this.down = null;
+	},
+	
+	keysDown: {},
+	
+	lastKeyEventProcessed: null,
+	
+	updateKeyInteraction: function()
+	{
+        var now = new Date().getTime();
+
+        if (this.lastKeyEventProcessed === null)
+        {
+            this.lastKeyEventProcessed = now;
+            return;
+        }
+
+        
+        var dt = now - this.lastKeyEventProcessed;
+        this.lastKeyEventProcessed = now;
+        
+        var arc = viewAngleYaw / 180 * Math.PI;
+        var forwardX = Math.sin(arc);
+        var forwardY = Math.cos(arc);
+
+        var rightX = Math.sin(arc + Math.PI/2.0);
+        var rightY = Math.cos(arc + Math.PI/2.0);
+        
+        if (this.keysDown.D) { localPosition.x += rightX * dt/400; localPosition.y += rightY * dt/400;};
+        if (this.keysDown.A) { localPosition.x -= rightX * dt/400; localPosition.y -= rightY * dt/400;};
+
+        if (this.keysDown.W) { localPosition.x += forwardX * dt/400; localPosition.y +=forwardY * dt/400;}
+        if (this.keysDown.S) { localPosition.x -= forwardX * dt/400; localPosition.y -=forwardY * dt/400;}
+	},
+	
+	x:null,
+	y:null,
+	//id of the touch event that is currently tracked as 'down'; "mouse" if the mouse is tracked, null if none
+    down: null,
+    
+	onKeyDown: function(evt) 
+	{
+        //console.log("Key event: key %s", evt.keyCode);
+        var key = null;
+        switch (evt.keyCode)
+        {
+            
+            case 65: key = "A"; break;
+            case 68: key = "D"; break;
+            case 83: key = "S"; break;
+            case 87: key = "W"; break;
+        }
+        
+        if (key in this.keysDown) //is just a reoccuring event for a key that is still pressed
+            return;
+            
+        this.updateKeyInteraction();
+        this.keysDown[key] = key;
+        
+        scheduleFrameRendering();
+    },
+
+	onKeyUp: function(evt)
+	{
+        //console.log("Key event: key %s", evt.keyCode);
+        switch (evt.keyCode)
+        {
+            
+            case 65: delete this.keysDown.A; break;
+            case 68: delete this.keysDown.D; break;
+            case 83: delete this.keysDown.S; break;
+            case 87: delete this.keysDown.W; break;
+        }
+    },
+    
+    keysStillPressed: function()
+    {
+        return Object.keys(this.keysDown).length > 0;
+    },
+	
+	updateViewDirection: function(dx, dy)
+	{
+        viewAngleYaw += dx/5 ;
+        viewAnglePitch += dy/ 5;
+        if (viewAnglePitch > 180)
+            viewAnglePitch -= 180;
+        
+        if (viewAnglePitch < -60)
+            viewAnglePitch = -60;
+            
+        if (viewAnglePitch > 60)
+            viewAnglePitch = 60;
+        /*localPosition.x += dx/100;
+        localPosition.y += dy/100;*/
+
+        scheduleUpdateHistoryState();
+        scheduleFrameRendering();
+	},
+	
+    onMouseMove: function(e)
+	{
+        if (this.down != "mouse" ) return;
+        var dx = e.clientX - this.x;
+        var dy = e.clientY - this.y;
+
+        this.x = e.clientX;
+        this.y = e.clientY;
+
+        //console.log("mouse move this: %s, %o", this, this);
+        this.updateViewDirection(dx, dy);
+	},
+
+    getTouchData: function(touches, identifier)
+    {
+        for (var i in touches)
+        {
+            if (touches[i].identifier == identifier)
+            {
+                return touches[i];
+            }
+        }
+        return null;
+    },
+    
+    onTouchDown: function(ev)
+    {
+        ev.preventDefault();
+        var touch = ev.changedTouches[0];
+        down = touch.identifier;
+        x = touch.clientX;
+        y = touch.clientY;
+    },
+    
+    onTouchEnd: function(ev)
+    {
+    
+        ev.preventDefault();
+        if (getTouchData(ev.changedTouches, down))
+        {
+            down = null;
+        }
+    },
+
+    onTouchMove: function(ev)
+    {
+    
+        ev.preventDefault();
+        var touch = getTouchData(ev.changedTouches, down);
+        if (touch)
+        {
+            var dx = touch.clientX - x;
+            var dy = touch.clientY - y;
+
+            if (Math.abs(dx) < 5 && Math.abs(dy) < 5)
+                return;
+            x = touch.clientX;
+            y = touch.clientY;
+            updateViewDirection(dx, dy);
+            
+        }
+    }
+}
