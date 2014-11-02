13a3812a8b0b680be01da67974e5e42f1837d8ce
Robert Buchholz <rbuch703@gmail.com>
2014-11-02 19:21:41 +0100

    - added parsing of building and roof colors

--- buildings.js
+++ buildings.js
@@ -412,7 +412,8 @@ Buildings.mergeMultiPolygonSegments = function(rel, setOfRelations) {
 
 //distributes the attributes that a relation may have, but its members may not, to these members
 Buildings.distributeAttributes = function(rel) {
-    var important_tags = ["building:levels", "roof:levels", "building:min_level", "height", "min_height"];
+    var important_tags = ["building:levels", "roof:levels", "building:min_level", "height", "min_height", 
+                          "building:colour", "building:color", "roof:colour", "roof:color"];
 
 
     if (! ("tags" in rel))
@@ -563,24 +564,92 @@ function getLengthInMeters(len_str) {
     return val;
 }
 
+//map of all default CSS color names
+Buildings.colors = {
+"aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aqua":"#00ffff","aquamarine":"#7fffd4","azure":"#f0ffff",
+"beige":"#f5f5dc","bisque":"#ffe4c4","black":"#000000","blanchedalmond":"#ffebcd","blue":"#0000ff",
+"blueviolet":"#8a2be2","brown":"#a52a2a","burlywood":"#deb887","cadetblue":"#5f9ea0","chartreuse":"#7fff00",
+"chocolate":"#d2691e","coral":"#ff7f50","cornflowerblue":"#6495ed","cornsilk":"#fff8dc","crimson":"#dc143c",
+"cyan":"#00ffff","darkblue":"#00008b","darkcyan":"#008b8b","darkgoldenrod":"#b8860b","darkgray":"#a9a9a9",
+"darkgreen":"#006400","darkkhaki":"#bdb76b","darkmagenta":"#8b008b","darkolivegreen":"#556b2f","darkorange":"#ff8c00",
+"darkorchid":"#9932cc","darkred":"#8b0000","darksalmon":"#e9967a","darkseagreen":"#8fbc8f","darkslateblue":"#483d8b",
+"darkslategray":"#2f4f4f","darkturquoise":"#00ced1","darkviolet":"#9400d3","deeppink":"#ff1493","deepskyblue":"#00bfff",
+"dimgray":"#696969","dodgerblue":"#1e90ff","firebrick":"#b22222","floralwhite":"#fffaf0","forestgreen":"#228b22",
+"fuchsia":"#ff00ff","gainsboro":"#dcdcdc","ghostwhite":"#f8f8ff","gold":"#ffd700","goldenrod":"#daa520",
+"gray":"#808080","green":"#008000","greenyellow":"#adff2f","honeydew":"#f0fff0","hotpink":"#ff69b4",
+"indianred":"#cd5c5c","indigo":"#4b0082","ivory":"#fffff0","khaki":"#f0e68c","lavender":"#e6e6fa",
+"lavenderblush":"#fff0f5","lawngreen":"#7cfc00","lemonchiffon":"#fffacd","lightblue":"#add8e6","lightcoral":"#f08080",
+"lightcyan":"#e0ffff","lightgoldenrodyellow":"#fafad2","lightgrey":"#d3d3d3","lightgreen":"#90ee90",
+"lightpink":"#ffb6c1","lightsalmon":"#ffa07a","lightseagreen":"#20b2aa","lightskyblue":"#87cefa",
+"lightslategray":"#778899","lightsteelblue":"#b0c4de","lightyellow":"#ffffe0","lime":"#00ff00","limegreen":"#32cd32",
+"linen":"#faf0e6","magenta":"#ff00ff","maroon":"#800000","mediumaquamarine":"#66cdaa","mediumblue":"#0000cd",
+"mediumorchid":"#ba55d3","mediumpurple":"#9370d8","mediumseagreen":"#3cb371","mediumslateblue":"#7b68ee",
+"mediumspringgreen":"#00fa9a","mediumturquoise":"#48d1cc","mediumvioletred":"#c71585","midnightblue":"#191970",
+"mintcream":"#f5fffa","mistyrose":"#ffe4e1","moccasin":"#ffe4b5","navajowhite":"#ffdead","navy":"#000080",
+"oldlace":"#fdf5e6","olive":"#808000","olivedrab":"#6b8e23","orange":"#ffa500","orangered":"#ff4500","orchid":"#da70d6",
+"palegoldenrod":"#eee8aa","palegreen":"#98fb98","paleturquoise":"#afeeee","palevioletred":"#d87093",
+"papayawhip":"#ffefd5","peachpuff":"#ffdab9","peru":"#cd853f","pink":"#ffc0cb","plum":"#dda0dd","powderblue":"#b0e0e6",
+"purple":"#800080","red":"#ff0000","rosybrown":"#bc8f8f","royalblue":"#4169e1","saddlebrown":"#8b4513",
+"salmon":"#fa8072","sandybrown":"#f4a460","seagreen":"#2e8b57","seashell":"#fff5ee","sienna":"#a0522d",
+"silver":"#c0c0c0","skyblue":"#87ceeb","slateblue":"#6a5acd","slategray":"#708090","snow":"#fffafa",
+"springgreen":"#00ff7f","steelblue":"#4682b4","tan":"#d2b48c","teal":"#008080","thistle":"#d8bfd8","tomato":"#ff6347",
+"turquoise":"#40e0d0","violet":"#ee82ee","wheat":"#f5deb3","white":"#ffffff","whitesmoke":"#f5f5f5","yellow":"#ffff00",
+"yellowgreen":"#9acd32"};
+
+Buildings.toneDownColor = function(col, fac, brighten)
+{
+    var l = 0.2126 * col[0] + 0.7152 * col[1] + 0.0722 * col[2];   //luminance
+    if (brighten)
+        l += 0.25;
+    return [ (1-fac)*col[0] + fac*l,
+              (1-fac)*col[1] + fac*l,
+              (1-fac)*col[2] + fac*l];
+}
+
+
+Buildings.interpretColor = function(col, defaultColor)
+{
+    if (col === undefined)
+        return defaultColor;
+        
+    if (col in Buildings.colors)
+        col = Buildings.colors[col];
+    
+    var re = new RegExp("^#([a-fA-F0-9]{6})$");   //a hash sign followed by exactly six hexadecimal characters
+    var m = col.match(re)
+    if (m)
+    {
+        col = m[1]; //m[0] returns the whole matched string, m[1] only the submatch in the first set of parentheses
+        //console.log(col);
+        var r = parseInt("" + col[0] + col[1], 16)/255.0;
+        var g = parseInt("" + col[2] + col[3], 16)/255.0;
+        var b = parseInt("" + col[4] + col[5], 16)/255.0;
+        return Buildings.toneDownColor([r, g, b], 0.5, true);
+    }
+    
+
+    return defaultColor;
+}
+
 Buildings.prototype.buildGlGeometry = function(outlines) {
     if (!gl)
         return;
         
         
     this.vertices= [];
+    this.vertexColors= [];
     this.texCoords=[];
     this.normals  =[];
     this.edgeVertices = [];
     
-    var vertexArrays = [];
-    var texCoordArrays = [];
-
     for (var i in outlines)
     {
         var bldg = outlines[i];
         if (! ("tags" in bldg))
             bldg.tags = {};
+            
+        //if ("building:colour" in bldg.tags)
+        //    console.log("building with color %s", bldg.tags["building:colour"]);
 
         if ("height" in bldg.tags)
             bldg.height = getLengthInMeters(bldg.tags.height);
@@ -614,6 +683,23 @@ Buildings.prototype.buildGlGeometry = function(outlines) {
         if (bldg.nodes[0].dx != bldg.nodes[bldg.nodes.length-1].dx || bldg.nodes[0].dy != bldg.nodes[bldg.nodes.length-1].dy)
             console.log("[WARN] outline of building %s does not form a closed loop (%o)", i, bldg);
         
+        bldg.color = [0.8, 0.8, 0.8];
+        bldg.roofColor = [0.8, 0.8, 0.8];
+        
+        //use incorrect tag name first, overwrite with correct one if it exists
+        bldg.color = Buildings.interpretColor(bldg.tags["building:color"], bldg.color);
+        bldg.color = Buildings.interpretColor(bldg.tags["building:colour"], bldg.color);    
+
+        bldg.roofColor = Buildings.interpretColor(bldg.tags["roof:color"], bldg.roofColor);
+        bldg.roofColor = Buildings.interpretColor(bldg.tags["roof:colour"], bldg.roofColor);    
+        
+        
+        if (bldg.height === undefined)
+        {
+            bldg.color = [0.9, 0.6, 0.6];
+            bldg.roofColor=[0.9, 0.6, 0.6];
+        }
+        
         //step 1: build geometry for walls;
         for (var j = 0; j < bldg.nodes.length - 1; j++) //loop does not include the final vertex, as we in each case access the successor vertex as well
         {
@@ -639,7 +725,10 @@ Buildings.prototype.buildGlGeometry = function(outlines) {
             var coords = [].concat(A, B, C, A, C, D);
             [].push.apply(this.vertices, coords);
             
-            var tc = [0,0,hf,   hDist,0,hf,   hDist,numLevels,hf,   0,0,hf,   hDist,numLevels,hf, 0,numLevels,hf];
+            var colors = [].concat(bldg.color, bldg.color, bldg.color, bldg.color, bldg.color, bldg.color);
+            [].push.apply( this.vertexColors, colors);
+            
+            var tc = [0,0,   hDist,0,   hDist,numLevels,   0,0,   hDist,numLevels,   0,numLevels];
             [].push.apply( this.texCoords, tc); //this 'hack' is way faster than concat()
             
             var norms = [].concat(N,N,N,N,N,N);
@@ -666,8 +755,9 @@ Buildings.prototype.buildGlGeometry = function(outlines) {
         {
             for (var j = 0; j < coords.length; j+=2)
             {
+                [].push.apply(this.vertexColors, bldg.color);
                 this.vertices.push(coords[j], coords[j+1], min_height);
-                this.texCoords.push(0.5, 0.5,hf);
+                this.texCoords.push(0.0, 0.0);
                 this.normals.push( 0,0,-1 ); //floor --> normal is pointing straight down
             }
 
@@ -687,14 +777,19 @@ Buildings.prototype.buildGlGeometry = function(outlines) {
             this.vertices.push(coords[j+4], coords[j+5], height);   //order reversed to change orientation
             this.vertices.push(coords[j+2], coords[j+3], height);
 
-            this.texCoords.push(0.5, 0.5,hf,  0.5, 0.5,hf,  0.5, 0.5,hf);
+            var colors = [].concat(bldg.roofColor, bldg.roofColor, bldg.roofColor);
+            [].push.apply( this.vertexColors, colors);
+
+
+            this.texCoords.push(0.5, 0.5,  0.5, 0.5,  0.5, 0.5);
             this.normals.push( 0,0,1,  0,0,1,  0,0,1 ); //roof --> normal is pointing straight up
         }
     }
     this.numVertices = this.vertices.length/3.0;    // 3 coordinates per vertex
     this.numEdgeVertices = this.edgeVertices.length/3.0;
-    console.log("'Buildings' total to %s faces and %s edges", this.numVertices/3, this.numEdgeVertices/2);
+    console.log("'Buildings' total to %s faces (%s colors), and %s edges", this.numVertices/3, this.vertexColors.length / 9.0, this.numEdgeVertices/2);
     this.vertices = glu.createArrayBuffer(this.vertices);
+    this.vertexColors = glu.createArrayBuffer(this.vertexColors);
     this.texCoords= glu.createArrayBuffer(this.texCoords);
     this.normals  = glu.createArrayBuffer(this.normals);
     this.edgeVertices = glu.createArrayBuffer(this.edgeVertices);
@@ -731,14 +826,21 @@ Buildings.prototype.render = function(modelViewMatrix, projectionMatrix) {
         
     //draw faces
 	gl.useProgram(Shaders.building);   //    Install the program as part of the current rendering state
-	gl.enableVertexAttribArray(Shaders.building.locations.vertexPosition); // setup vertex coordinate buffer
-	gl.enableVertexAttribArray(Shaders.building.locations.vertexTexCoords); //setup texcoord buffer
 
+	gl.enableVertexAttribArray(Shaders.building.locations.vertexPosition); // setup vertex coordinate buffer
     gl.bindBuffer(gl.ARRAY_BUFFER, this.vertices);   //select the vertex buffer as the currrently active ARRAY_BUFFER (for subsequent calls)
 	gl.vertexAttribPointer(Shaders.building.locations.vertexPosition, 3, gl.FLOAT, false, 0, 0);  //assigns array "vertices" bound above as the vertex attribute "vertexPosition"
     
+	gl.enableVertexAttribArray(Shaders.building.locations.vertexTexCoords); //setup texcoord buffer
 	gl.bindBuffer(gl.ARRAY_BUFFER, this.texCoords);
-	gl.vertexAttribPointer(Shaders.building.locations.vertexTexCoords, 3, gl.FLOAT, false, 0, 0);  //assigns array "texCoords" bound above as the vertex attribute "vertexTexCoords"
+	gl.vertexAttribPointer(Shaders.building.locations.vertexTexCoords, 2, gl.FLOAT, false, 0, 0);  //assigns array "texCoords" bound above as the vertex attribute "vertexTexCoords"
+
+    if (Shaders.building.locations.vertexColorIn > -1)
+    {
+    	gl.enableVertexAttribArray(Shaders.building.locations.vertexColorIn); //setup texcoord buffer
+	    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexColors);
+	    gl.vertexAttribPointer(Shaders.building.locations.vertexColorIn, 3, gl.FLOAT, false, 0, 0);
+	}
 
     // can apparently be -1 if the variable is not used inside the shader
     if (Shaders.building.locations.vertexNormal > -1)
